c071879edd3ced4749851777ccdb40088c83551b
Ольга Чулакова <olka.13@list.ru>
2025-03-20 13:17:39 +0300

    добавляет все разметку

--- source/js/main.js
+++ source/js/main.js
@@ -2,23 +2,5 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
-// const detailsElements = document.querySelectorAll('.accordion__content');
-
-// function setupAccordionAnimation() {
-
-//   detailsElements.forEach((details) => {
-//     details.addEventListener('toggle', function() {
-//       const content = this.querySelector('.accordion__body');
-//       if (this.open) {
-//         content.style.maxHeight = `${content.scrollHeight }px`;
-//       } else {
-//         content.style.maxHeight = null;
-//       }
-//     });
-//   });
-// }
-
-
-// setupAccordionAnimation();
 
 
